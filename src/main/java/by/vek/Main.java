6f7f55df0a9742555fd688daeb113a0255564ce9
Daniil.Vasilenka <daniil.vasilenko.10@mail.ru>
2025-07-23 20:13:20 +0300

    final task

--- src/main/java/by/vek/Main.java
+++
@@ -1,8 +0,0 @@
-package by.vek;
-
-public class Main {
-    public static void main(String[] args) {
-        String text = Utils.readFile("Body-login.JSON");
-        System.out.println(text);
-    }
-}
